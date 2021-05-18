2d88704870f2a4281db05ca7f0d21bed8b6a0d4f
fabricio-basso <fabricio.basso12@gmail.com>
2021-05-17 22:20:15 -0300

    commit (:

---
+++ Assets/AI.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using Panda;
+
+public class AI : MonoBehaviour
+{
+
+
+
+
+    // position player
+    public Transform player;   
+    // position tiro
+    public Transform bulletSpawn;
+    // ref do bullet
+    public GameObject bulletPrefab;
+    // components do Slider
+    public Slider healthBar;
+
+    // Navmesh deste objeto
+    NavMeshAgent agent;
+
+    public Vector3 destination;
+    public Vector3 target;
+
+    //var para a vida 
+    float health = 100.0f;
+    
+    //var veloc da rotação
+    float rotSpeed = 5.0f;
+
+
+    float shotRange = 40.0f;
+    float visibleRange = 80.0f;
+
+    void Start()
+    {
+        //pega os components do object
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = shotRange - 5; 
+
+        //Funçao para mostrar vida
+        InvokeRepeating("UpdateHealth",5,0.5f);
+    }
+
+
+    void Update()
+    {
+        //deixa a barra de hp em cima do boneco
+        Vector3 healthBarPos = Camera.main.WorldToScreenPoint(this.transform.position);
+
+        healthBar.value = (int)health;
+        healthBar.transform.position = healthBarPos + new Vector3(0,60,0);
+    }
+
+    void UpdateHealth()
+    {
+        //Regeneraçao
+       if(health < 100)
+        {
+            health ++;
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        //removendo vida pela colisão
+        if(col.gameObject.tag == "bullet")
+        {
+            health -= 10;
+        }
+
+    }
+
+    [Task]
+    public void PickRandomDestination()
+    {
+        //navegacao com Panda
+        Vector3 dest = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100));
+        agent.SetDestination(dest);
+        Task.current.Succeed();
+    }
+
+    [Task]
+    public void MoveToDestination()
+    {
+        //deslocamento com Panda
+        if(Task.isInspected)
+            Task.current.debugInfo = string.Format("t={0:0.00}", Time.time);
+
+        if(agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+        {
+            Task.current.Succeed();
+        }
+
+    }
+
+
+}
+
---
+++ Assets/Drive.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour 
+{
+    //var de velocidade
+	float speed = 20.0F;
+    //
+
+    //var de velocidade da rotacao
+    float rotationSpeed = 120.0F;
+    //
+
+    //prefab da bullet(tiro)
+    public GameObject bulletPrefab;
+    //get posicao da bullet
+    public Transform bulletSpawn;
+
+    void Update() {
+        //movimentação com os Inputs da propria Engine(UNITY)
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+
+        //deslocamento do player
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        //tiro
+        if(Input.GetKeyDown("space"))
+        {
+            GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward*2000);
+
+        }
+    }
+
+}
